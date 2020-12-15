5b05ea070271979b25f425a1359f2f5901ee9f32
Sam K <retaildiamond@gmail.com>
2020-12-14 23:06:05 -0500

    idk part 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+// import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 import './App.css';
 
 function App() {
